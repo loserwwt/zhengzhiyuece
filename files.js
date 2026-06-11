ffd8d83395f12c0a55d1d49d7027c9689c4dc281
loserwwt <loserwwt@gmail.com>
2026-06-11 20:23:17 +0800

    Add files via upload

--- files.js
+++ files.js
@@ -1,5 +1,5 @@
 ﻿window.DOWNLOAD_META = {
-  generatedAt: "2026-06-11 20:12",
+  generatedAt: "2026-06-11 20:22",
 };
 
 window.DOWNLOAD_FILES = [
@@ -10,16 +10,6 @@ window.DOWNLOAD_FILES = [
         "bytes":  26029,
         "modified":  "2026-06-11 08:45",
         "modifiedIso":  "2026-06-11T08:45:17",
-        "description":  null,
-        "extension":  "xlsx"
-    },
-    {
-        "name":  "5月学生政治理论学习月测（自测）_final.xlsx",
-        "path":  "downloads/5%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89_final.xlsx",
-        "size":  "24.9 KB",
-        "bytes":  25499,
-        "modified":  "2026-05-09 10:51",
-        "modifiedIso":  "2026-05-09T10:51:14",
         "description":  null,
         "extension":  "xlsx"
     }
