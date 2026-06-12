575ba6149156dc93171e605252a9a4779ace47d8
loserwwt <loserwwt@gmail.com>
2026-06-12 11:53:44 +0800

    Add files via upload

--- files.js
+++ files.js
@@ -1,26 +1,26 @@
 ﻿window.DOWNLOAD_META = {
-  generatedAt: "2026-06-11 20:41",
+  generatedAt: "2026-06-12 11:51",
 };
 
 window.DOWNLOAD_FILES = [
     {
-        "name":  "6月学生政治理论学习月测（自测）_final.xlsx",
-        "path":  "downloads/6%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89_final.xlsx",
+        "name":  "6月学生政治理论学习月测（自测）.xlsx",
+        "path":  "downloads/6%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89.xlsx",
         "size":  "25.4 KB",
-        "bytes":  26029,
-        "modified":  "2026-06-11 08:45",
-        "modifiedIso":  "2026-06-11T08:45:17",
+        "bytes":  26010,
+        "modified":  "2026-06-12 11:49",
+        "modifiedIso":  "2026-06-12T11:49:51",
         "description":  null,
         "extension":  "xlsx"
     },
     {
-        "name":  "5月学生政治理论学习月测（自测）_final.xlsx",
-        "path":  "downloads/5%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89_final.xlsx",
-        "size":  "24.9 KB",
-        "bytes":  25499,
-        "modified":  "2026-05-09 10:51",
-        "modifiedIso":  "2026-05-09T10:51:14",
+        "name":  "6月学生政治理论学习月测（自测）_阅读版.txt",
+        "path":  "downloads/6%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89_%E9%98%85%E8%AF%BB%E7%89%88.txt",
+        "size":  "35.7 KB",
+        "bytes":  36530,
+        "modified":  "2026-06-12 11:49",
+        "modifiedIso":  "2026-06-12T11:49:51",
         "description":  null,
-        "extension":  "xlsx"
+        "extension":  "txt"
     }
 ];
