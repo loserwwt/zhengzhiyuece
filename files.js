c845c00ef88d80077157c978161af7ae28fbf809
loserwwt <loserwwt@gmail.com>
2026-07-09 20:34:17 +0800

    Add files via upload

--- files.js
+++ files.js
@@ -1,45 +1,25 @@
 ﻿window.DOWNLOAD_META = {
-  generatedAt: "2026-06-27 12:14",
+  generatedAt: "2026-07-09 20:33",
 };
 
 window.DOWNLOAD_FILES = [
     {
-        "name":  "6新题.xlsx",
-        "path":  "downloads/6%E6%96%B0%E9%A2%98.xlsx",
-        "size":  "12.8 KB",
-        "bytes":  13110,
-        "modified":  "2026-06-27 12:09",
-        "modifiedIso":  "2026-06-27T12:09:40",
+        "name":  "7月学生政治理论学习月测（自测）.xlsx",
+        "path":  "downloads/7%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89.xlsx",
+        "size":  "44.4 KB",
+        "bytes":  45440,
+        "modified":  "2026-07-09 20:30",
+        "modifiedIso":  "2026-07-09T20:30:46",
         "description":  null,
         "extension":  "xlsx"
     },
     {
-        "name":  "6新题.txt",
-        "path":  "downloads/6%E6%96%B0%E9%A2%98.txt",
-        "size":  "5.6 KB",
-        "bytes":  5744,
-        "modified":  "2026-06-27 12:08",
-        "modifiedIso":  "2026-06-27T12:08:17",
-        "description":  null,
-        "extension":  "txt"
-    },
-    {
-        "name":  "6月学生政治理论学习月测（自测）.xlsx",
-        "path":  "downloads/6%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89.xlsx",
-        "size":  "25.4 KB",
-        "bytes":  26010,
-        "modified":  "2026-06-12 11:49",
-        "modifiedIso":  "2026-06-12T11:49:51",
-        "description":  null,
-        "extension":  "xlsx"
-    },
-    {
-        "name":  "6月学生政治理论学习月测（自测）_阅读版.txt",
-        "path":  "downloads/6%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89_%E9%98%85%E8%AF%BB%E7%89%88.txt",
-        "size":  "35.7 KB",
-        "bytes":  36530,
-        "modified":  "2026-06-12 11:49",
-        "modifiedIso":  "2026-06-12T11:49:51",
+        "name":  "7月学生政治理论学习月测（自测）_阅读版.txt",
+        "path":  "downloads/7%E6%9C%88%E5%AD%A6%E7%94%9F%E6%94%BF%E6%B2%BB%E7%90%86%E8%AE%BA%E5%AD%A6%E4%B9%A0%E6%9C%88%E6%B5%8B%EF%BC%88%E8%87%AA%E6%B5%8B%EF%BC%89_%E9%98%85%E8%AF%BB%E7%89%88.txt",
+        "size":  "71.0 KB",
+        "bytes":  72754,
+        "modified":  "2026-07-09 20:30",
+        "modifiedIso":  "2026-07-09T20:30:46",
         "description":  null,
         "extension":  "txt"
     }
